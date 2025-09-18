7c128a007a192e269d64ebbf994a493abc395e77
JonathanVialpando <109265169+JonathanVialpando@users.noreply.github.com>
2025-09-17 17:44:36 -0700

    Setup Functions Problem 1

--- main.py
+++ main.py
@@ -1,16 +1,71 @@
-# This is a sample Python script.
+# Project 1
+# Jonathan Vialpando and Arturo Nunez Gomez
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+# Input:
+# Output:
+# Pre-condition of input:
+# Pseudo Code:
+#def gcd(a,b)
+
+# Input:
+# Output:
+# Pre-condition of input:
+# Pseudo Code:
+#def reduceFraction(p,q)
+
+# Input:
+# Output:
+# Pre-condition of input:
+# Pseudo Code:
+#def add(p1, q1, p2, q2)
+
+# Input:
+# Output:
+# Pre-condition of input:
+# Pseudo Code:
+#def subtract(p1, q1, p2, q2)
+
+# Input:
+# Output:
+# Pre-condition of input:
+# Pseudo Code:
+#def multiply(p1, q1, p2, q2)
+
+# Input:
+# Output:
+# Pre-condition of input:
+# Pseudo Code:
+#def divide(p1, q1, p2, q2)
+
+# Input:
+# Output:
+# Pre-condition of input:
+# Pseudo Code:
+#def isEqual(p1, q1, p2, q2)
+
+# Input:
+# Output:
+# Pre-condition of input:
+# Pseudo Code:
+#def isLess(p1, q1, p2, q2)
+
+# Input:
+# Output:
+# Pre-condition of input:
+# Pseudo Code:
+#def digitOfFraction(p, q, n)
+
+# Input:
+# Output:
+# Pre-condition of input:
+# Pseudo Code:
+#def hsum(n ,m)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
 
-# Press the green button in the gutter to run the script.
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
